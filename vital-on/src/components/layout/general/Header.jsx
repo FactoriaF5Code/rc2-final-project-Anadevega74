1a584999bdb504c92fb44c5d883f7ffc2c121f17
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-03-22 21:02:51 +0100

    Header and nav added

---
+++ vital-on/src/components/layout/general/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Nav } from "./Nav";
+
+export const Header = () => {
+  return (
+    <header className="layout__navbar">
+      <img className="logo" src="./assets/img/logo sin fondo.png" alt="logo" />
+      <div className="navbar__header"></div>
+
+      <Nav />
+    </header>
+  );
+};
