55c792b3a3ca3fd636a5e6efaa9b0e4c45942f96
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-04-01 00:31:28 +0200

    Connected database

---
+++ api-rest-vital-on/arrancar-proyecto.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:/data5/db"
+start cmd.exe /k "cd C:\C:\Users\Usuario\Documents\factoriaf5\rc2fullstack\projets\rc2-final-project-Anadevega74\rc2-final-project-Anadevega74\api-rest-vital-on && npm start"
+
---
+++ api-rest-vital-on/index.js
@@ -0,0 +1,20 @@
+//Importar dependencias
+const connection = require("./database/connection");
+
+//Mensaje bienvenida
+console.log("API NODE para RED SOCIAL arrancada");
+
+//Conexión bbdd
+connection();
+
+//Crear servidor node
+
+
+//Configurar cors
+
+
+//Convertir datos del body a objetos js
+
+//Cargar conf rutas
+
+//Poner servidor a escuchar peticiones http
